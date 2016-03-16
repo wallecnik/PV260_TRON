30750ae016d1fb7be9b21c2f9f29b232bfc8dc49
tomas.valka <tomas.valka@mycroftmind.com>
2016-03-16 18:31:15 +0100

    Player creation

--- src/Player.java
+++ src/Player.java
@@ -1,19 +1,67 @@
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Wallecnik on 16.03.16.
  */
 public class Player {
 
-    final int centrex;
-    final int centrey;
-    final int currentDirection;
-    final ArrayList<Integer> pathx1 = new ArrayList<>();
-    final ArrayList<Integer> pathy1 = new ArrayList<>();
+    private final static int SPEED = 5;
 
-    public Player(int centrex, int centrey, int currentDirection) {
-        this.centrex = centrex;
-        this.centrey = centrey;
+    private Point position;
+    private Direction currentDirection;
+    private final List<Point> placesVisited = new ArrayList<>();
+
+    public Player(Point origin, Direction currentDirection) {
+        this.position = origin;
         this.currentDirection = currentDirection;
+
+        this.placesVisited.add(this.position);
+    }
+
+    public void move() {
+        switch(currentDirection){
+            case DOWN:
+                this.position = this.position.toDown(SPEED);
+                break;
+            case RIGHT:
+                this.position = this.position.toRight(SPEED);
+                break;
+            case UP:
+                this.position = this.position.toUp(SPEED);
+                break;
+            case LEFT:
+                this.position = this.position.toLeft(SPEED);
+                break;
+        }
+
+        this.placesVisited.add(this.position);
+    }
+
+    private enum Direction {
+        UP, DOWN, LEFT, RIGHT;
+    }
+
+    public static class Point {
+        private final int x;
+        private final int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Point toUp(int speed) {
+            return new Point(this.x, this.y + speed);
+        }
+        public Point toDown(int speed) {
+            return new Point(this.x, this.y - speed);
+        }
+        public Point toLeft(int speed) {
+            return new Point(this.x - speed, this.y);
+        }
+        public Point toRight(int speed) {
+            return new Point(this.x + speed, this.y);
+        }
     }
 }
